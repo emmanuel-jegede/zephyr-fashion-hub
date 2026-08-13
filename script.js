8049aaaa587b061de7f71613846cc4ea4cd6dee3
emmanuel-jegede <emmanuelibukunoluwa2010@gmail.com>
2026-08-13 07:40:01 +0100

    Updated files

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.classList.remove("active");
 }
+   const filterButtons = document.querySelectorAll(".filter-btn");
+    const collectionItems = document.querySelectorAll(".collection-item");
+    
+    filterButtons.forEach(button => {
+      
+      button.addEventListener("click", () => {
+        
+        const filter = button.dataset.filter;
+        
+        // Remove active from all buttons
+        filterButtons.forEach(btn => {
+          btn.classList.remove("active");
+        });
+        
+        // Add active to clicked button
+        button.classList.add("active");
+        
+        // Show or hide products
+        collectionItems.forEach(item => {
+          
+          if (filter === "all" || item.classList.contains(filter)) {
+            item.style.display = "block";
+          } else {
+            item.style.display = "none";
+          }
+          
+        });
+        
+      });
+      
+    });
